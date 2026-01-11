d8a24ee02c523cac6a55dcdfc9aca08700fb636e
Shreya Wani <shreyawani2006@gmail.com>
2026-01-11 23:34:09 +0530

    Backend server + DB connection structure is ready

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import { connect } from 'mongoose';
 
 dotenv.config({
     path: './.env'
 })
-
 const PORT = process.env.PORT || 8000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database", err);
+        process.exit(1);
+    })
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
