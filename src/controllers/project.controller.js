66b7a6f022e6fc57525d6bd08471d608f7e5391d
Shreya Wani <shreyawani2006@gmail.com>
2026-01-21 03:18:16 +0530

    feat(project): implement project CRUD operations

---
+++ src/controllers/project.controller.js
@@ -0,0 +1,153 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { Project } from "../models/project.model.js";
+import { ProjectMember } from "../models/projectMember.model.js";
+import { User } from "../models/user.model.js";
+import { UserRolesEnum } from "../utils/constants.js";
+
+const getProjects = asyncHandler(async (req, res) => {
+    const projects = await ProjectMember.find({ user: req.user._id})
+        .populate("project");
+        selectFields("project")
+    
+    return res.status(200).json(
+        new ApiResponse(200, projects, "Projects fetched successfully")
+    );
+});
+
+const getProjectById = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+        throw new ApiError(400, "Project ID is required");
+    }
+
+    const projectMember = ProjectMember.findOne({
+        project: projectId,
+        user: req.user._id
+    }).populate("project");
+
+    if (!projectMember) {
+        throw new ApiError(403, "You do not have access to this project");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            projectMember.project,
+            "Project fetched successfully"
+        )
+    );
+});
+
+const createProject = asyncHandler(async (req, res) => {
+    const { name, description } = req.body;
+
+    if (!name) {
+        throw new ApiError(400, "Project name is required");
+    }
+
+    //create project
+    const project = await Project.create({
+        name,
+        description,
+        createdBy: req.user._id
+    });
+
+    //add creater as admin of project
+    await ProjectMember.create({
+        project: project._id,
+        user: req.user._id,
+        role: UserRolesEnum.ADMIN
+        })
+
+    return res.status(200).json(
+        new ApiResponse(201, project, "Project created successfully")
+    )
+
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+    const { projectId }  = req.params;
+    const { name, description } = req.body;
+
+    if(!projectId){
+        throw new ApiError(400, "Project ID is required")
+    }
+
+    if(!name && !description) {
+        throw new ApiError(400, "At least one field is required to update")
+    }
+
+    const project = await Project.findByIdAndUpdate(
+        projectId,
+        { name, description },
+        { new: true}
+    );
+
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            project,
+            "Project updated successfully"
+        )
+    );
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+        throw new ApiError(400, "Project ID is required");
+    }
+
+    //delete project
+    const project = await Project.findByIdAndDelete(projectId);
+
+    if (!project) {
+        throw new ApiError(404, "Project not found");
+    }
+
+    //remove all members of this project
+    await ProjectMember.deleteMany({
+        project: projectId
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            null,
+            "Project deleted successfully"
+        )
+    );
+});
+
+const addMemberToProject = asyncHandler(async (req, res) => {
+    const { email, username, password, role } = req.body
+    
+    //validation
+});
+
+const getProjectMembers = asyncHandler(async (req, res) => {
+    const { email, username, password, role } = req.body
+    
+    //validation
+});
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+    const { email, username, password, role } = req.body
+    
+    //validation
+});
+
+const deleteMemberRole = asyncHandler(async (req, res) => {
+    const { email, username, password, role } = req.body
+    
+    //validation
+});
