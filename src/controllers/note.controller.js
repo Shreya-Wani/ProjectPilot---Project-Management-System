df1af68c4a0d66e7436e7ca23ca30b6102142956
Shreya Wani <shreyawani2006@gmail.com>
2026-01-20 23:53:59 +0530

    feat(note): fetch project notes with validation and population

---
+++ src/controllers/note.controller.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { Note } from "../models/note.model.js";
+import mongoose from "mongoose";
+
+//get all notes
+const getNotes = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        throw new ApiError(400, "Invalid project id");
+    }
+
+    const project = await project.findById(projectId);
+
+    if(!project){
+        throw new ApiError(404, "Project not found");
+    }
+
+    const notes = await Note.find({
+        project: projectId
+    }).populate("createdBy", "username fullname avatar")
+      .sort({ createdAt: -1 })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, notes, "Notes fetched successfully"))
+});
