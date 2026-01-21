8a58e3d415f9aaff6b7996eb337dec1da6e1f3b3
Shreya Wani <shreyawani2006@gmail.com>
2026-01-22 01:31:47 +0530

    feat(task): add task CRUD controllers

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -41,3 +41,80 @@ const createTask = asyncHandler(async (req, res) => {
     );
 });
 
+//get tasks by projets
+const getTaskByProject = asyncHandler(async (req, res) => {
+    const {projectId} = req.params;
+
+    if(!projectId){
+        throw new ApiError(400, "Projects is required");
+    }
+
+    const tasks = await Task.find({
+        project: projectId
+    }).populate("assignedTo", "username email")
+      .populate("assignedBy", "username email")
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json(
+        new ApiResponse(200,tasks,"Tasks fetched successfully")
+    );
+});
+
+//update task
+const updateTask = asyncHandler(async (req,res) => {
+    const { taskId } = req.params;
+    const {title, description, status, assignTo } = req.body;
+
+    if (!taskId) {
+        throw new ApiError(400, "Task ID is required");
+    }
+
+    const task = await Task.findById(taskId);
+    if(!task){
+        throw new ApiError(404, "Task not found");
+    }
+
+    // if reassigned, ensure new user is a project member
+    if(assignTo) {
+        const isMember = await ProjectMember.findOne({
+            project: task.project,
+            user: assignTo
+        });
+
+        if(!isMember) {
+            throw new ApiError(400, "Assigned user is not a member of this project")
+        }
+
+        task.assignedTo = assignTo;
+    }
+
+    if(title) task.title = title;
+    if(description) task.description = description;
+    if(status) task.status = status;
+
+    await task.save();
+
+    return res.status(200).json(
+        new ApiResponse(200, task, "Task updated successfully")
+    );
+});
+
+//delete task
+const deleteTask = asyncHandler(async (req, res) => {
+    const {taskId} = req.params;
+
+    if (!taskId) {
+        throw new ApiError(400, "Task ID is required");
+    }
+
+    const task = await Task.findById(taskId);
+    if(!task){
+        throw new ApiError(404, "Task not found");
+    }
+
+    await Task.findByIdAndDelete(taskId);
+
+    return res.status(200).json(
+        new ApiResponse(200, {}, "Task deleted successfully")
+    );
+});
